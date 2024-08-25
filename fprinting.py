f2b5d245847b6d2ad3e8ce240f7ef443d0d9cc30
doritgy <63942750+doritgy@users.noreply.github.com>
2024-08-25 18:48:03 +0300

    Add files via upload

---
+++ fprinting.py
@@ -0,0 +1,12 @@
+name1:str = "Alice";
+age1: int = 30;
+#            12345678911
+name2:str = "Laura Croft";
+age2: int = 27;
+print(f"Name: {name1} {age1}")
+print(f"Name: {name2} {age2}")
+print(f"Name: {name1:<11} {age1:>4}")
+print(f"Name: {name1:<11} {age1:^5}")
+print(f"Name: {name2:<11} {age2}")
+prc: float = 0.5;
+print(f"percentage: {prc:%}");
